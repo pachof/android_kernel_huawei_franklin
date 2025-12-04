693699f88978f52380a41df7c5caa0467b95a721
pachof <85188243+pachof@users.noreply.github.com>
2025-12-04 01:10:05 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,8 +31,10 @@ sudo -E apt-get -qq update
 sudo -E apt-get -qq install bc python3 python-is-python3 wget
 
 wget http://archive.ubuntu.com/ubuntu/pool/universe/p/python2.7/python2.7_2.7.18-13ubuntu1_amd64.deb
+wget https://archive.ubuntu.com/ubuntu/pool/universe/p/python2.7/libpython2.7-stdlib_2.7.18-1~20.04.7_amd64.deb
+wget https://archive.ubuntu.com/ubuntu/pool/universe/p/python2.7/python2.7-minimal_2.7.18-1~20.04.7_amd64.deb
 
-sudo -E apt -qq install ./python2.7_2.7.18-13ubuntu1_amd64.deb
+sudo -E apt -qq install ./python2.7_2.7.18-13ubuntu1_amd64.deb ./python2.7-minimal_2.7.18-1~20.04.7_amd64.deb ./libpython2.7-stdlib_2.7.18-1~20.04.7_amd64.deb
 if ! [ -d "${CLANG_DIR}" ]; then
 echo "Clang not found! Cloning to ${TC_DIR}..."
 if ! git clone --depth=1 -b clang-20.0 https://gitlab.com/kutemeikito/rastamod69-clang ${CLANG_DIR}; then

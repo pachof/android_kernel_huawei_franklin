08c0d3d784af8c2c20414b30168fa3ecf134bc5d
pachof <85188243+pachof@users.noreply.github.com>
2025-12-04 00:52:03 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -69,6 +69,8 @@ fi
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
+alias python=python2
+
 echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
 
